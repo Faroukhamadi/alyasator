6ff5ee2725d73e748e501ed76b10e719ecc3e2c0
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-17 15:23:44 +0100

    put files into package

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,58 @@
+use requestty::{Answers, ErrorKind, Question};
+
 pub struct Config {
     pub shell: String,
     pub is_permanent: bool,
     pub alias: String,
     pub original: String,
+}
+
+fn answers() -> Result<Answers, ErrorKind> {
+    let shell_select = Question::select("shell")
+        .message("What is your default shell?")
+        .choices(vec!["Bash", "Zsh", "Fish"])
+        .build();
+
+    let is_permanent_confirm = Question::confirm("permanent")
+        .message("Do you want the alias to be permanent?")
+        .build();
+
+    let alias_input = Question::input("alias")
+        .message("What is the alias you want to assign?")
+        .build();
+
+    let original_input = Question::input("original")
+        .message("What is the original command?")
+        .build();
+
+    let answers = requestty::prompt(vec![
+        shell_select,
+        is_permanent_confirm,
+        alias_input,
+        original_input,
+    ]);
+    answers
+}
+
+impl Config {
+    pub fn build() -> Result<Config, &'static str> {
+        match answers() {
+            Ok(answers) => {
+                let shell_answer = answers.get("shell").unwrap().clone();
+                let is_permanent_answer = answers.get("permanent").unwrap().clone();
+                let alias_answer = answers.get("alias").unwrap().clone();
+                let original_answer = answers.get("original").unwrap().clone();
+
+                Ok(Config {
+                    shell: shell_answer.as_list_item().unwrap().to_owned().text,
+                    is_permanent: is_permanent_answer.as_bool().unwrap(),
+                    alias: alias_answer.as_string().unwrap().to_owned(),
+                    original: original_answer.as_string().unwrap().to_owned(),
+                })
+            }
+            Err(_) => {
+                return Err("Interrupted");
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::process;
 
 use alyasator::Config;
-use requestty::{prompt::style::Stylize, Answers, ErrorKind, Question};
 
 fn main() {
-    let config = build().unwrap_or_else(|err| {
+    let config = Config::build().unwrap_or_else(|err| {
         eprintln!("Problem getting answers: {err}");
         process::exit(1);
     });
@@ -13,52 +12,4 @@ fn main() {
     println!("config's is_permanent: {}", config.is_permanent);
     println!("config's alias: {}", config.alias);
     println!("config's original: {}", config.original);
-}
-
-fn build() -> Result<Config, &'static str> {
-    match answers() {
-        Ok(answers) => {
-            let shell_answer = answers.get("shell").unwrap().clone();
-            let is_permanent_answer = answers.get("permanent").unwrap().clone();
-            let alias_answer = answers.get("alias").unwrap().clone();
-            let original_answer = answers.get("original").unwrap().clone();
-
-            Ok(Config {
-                shell: shell_answer.as_list_item().unwrap().to_owned().text,
-                is_permanent: is_permanent_answer.as_bool().unwrap(),
-                alias: alias_answer.as_string().unwrap().to_owned(),
-                original: original_answer.as_string().unwrap().to_owned(),
-            })
-        }
-        Err(_) => {
-            return Err("couldn't parse answers");
-        }
-    }
-}
-
-fn answers() -> Result<Answers, ErrorKind> {
-    let shell_select = Question::select("shell")
-        .message("What is your default shell?")
-        .choices(vec!["Bash", "Zsh", "Fish"])
-        .build();
-
-    let is_permanent_confirm = Question::confirm("permanent")
-        .message("Do you want the alias to be permanent?")
-        .build();
-
-    let alias_input = Question::input("alias")
-        .message("What is the alias you want to assign?")
-        .build();
-
-    let original_input = Question::input("original")
-        .message("What is the original command?")
-        .build();
-
-    let answers = requestty::prompt(vec![
-        shell_select,
-        is_permanent_confirm,
-        alias_input,
-        original_input,
-    ]);
-    answers
 }
