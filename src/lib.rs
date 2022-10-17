136bd2b7f6c1208f029250308f5298af9ad8ff07
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-17 01:52:40 +0100

    add answer match

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub struct Config {
+    pub shell: &'static str,
+    pub is_permanent: bool,
+    pub alias: String,
+    pub alias_value: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use requestty::{Answer, ListItem, Question};
+
 fn main() {
-    println!("Hello, world!");
+    let shell_select = Question::select("shell")
+        .message("What is your default shell?")
+        .choices(vec!["Bash", "Zsh", "Fish"])
+        .build();
+
+    let is_permanent_confirm = Question::confirm("permanent")
+        .message("Do you want the alias to be permanent?")
+        .build();
+
+    let answers = requestty::prompt(vec![shell_select, is_permanent_confirm]).unwrap();
+
+    for (_, answer) in answers {
+        match answer {
+            Answer::String(s) => println!("{}", s),
+            Answer::Bool(l) => println!("{}", l),
+            Answer::ListItem(l) => println!("{}", l.text),
+
+            _ => (),
+        }
+    }
 }
